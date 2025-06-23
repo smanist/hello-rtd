253a5b2349aaf882cfd3afd49ec8acb017cf3c47
Daning Huang <huangdn.nuaa@gmail.com>
2025-06-22 21:24:53 -0400

    Modify conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,11 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
